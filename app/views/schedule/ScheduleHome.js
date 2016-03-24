8c162a61a90a486f5f2a7d25e59dced13cd2d754
Joseph Roque <contact@josephroque.ca>
2016-03-23 21:17:22 -0700

    Adding buttons to add/remove courses

--- app/views/schedule/ScheduleHome.js
+++ app/views/schedule/ScheduleHome.js
@@ -7,7 +7,9 @@
 const React = require('react-native');
 const {
   Component,
+  Dimensions,
   Platform,
+  LayoutAnimation,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -16,11 +18,15 @@ const {
 
 const Configuration = require('../../util/Configuration');
 const Constants = require('../../Constants');
+const Icon = require('react-native-vector-icons/MaterialIcons');
 const LanguageUtils = require('../../util/LanguageUtils');
 const Preferences = require('../../util/Preferences');
 const SectionHeader = require('../../components/SectionHeader');
 const StatusBar = require('../../util/StatusBar');
 const Styles = require('../../Styles');
+
+const {height, width} = Dimensions.get('window');
+const screenWidth = width;
 
 class ScheduleHome extends Component {
 
@@ -39,19 +45,41 @@ class ScheduleHome extends Component {
     super(props);
     this.state = {
       dataSource: null,
+      showEditButtons: false,
     };
-
-    // Explicitly binding 'this' to all methods that need it
-    this._changeSchedule = this._changeSchedule.bind(this);
   };
 
   /*
    * Switches to the next available schedule and updates the views.
    */
-  _changeSchedule() {
+  _changeSemester() {
     Preferences.setToNextSemester();
     let header = this.refs.ScheduleHeader;
     header.updateSubtitle(LanguageUtils.getTranslatedName(Preferences.getSelectedLanguage(), Preferences.getCurrentSemesterInfo()), header.getSubtitleIcon(), header.getSubtitleIconClass());
+  };
+
+  /*
+   * Toggles the visibility of buttons for editing the schedule with animation.
+   */
+  _toggleEditScheduleButtons() {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    this.setState({
+      showEditButtons: !this.state.showEditButtons,
+    });
+  };
+
+  /*
+   * Opens a prompt for a user to add a new course.
+   */
+  _addCourse() {
+    // TODO: add a new course
+  };
+
+  /*
+   * Opens a prompt for a user to remove a course.
+   */
+  _removeCourse() {
+    // TODO: remove an old course
   };
 
   /*
@@ -74,6 +102,39 @@ class ScheduleHome extends Component {
       calendarIcon = ['material', 'event'];
     }
 
+    let buttons = null;
+    if (this.state.showEditButtons) {
+      buttons = (
+        <View style={_styles.editButtonContainer}>
+          <TouchableOpacity onPress={this._addCourse.bind(this)}>
+            <View style={[_styles.editButton, {margin: 10}]}>
+              <Icon name={'add'} size={24} color={'white'} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._removeCourse.bind(this)}>
+            <View style={[_styles.editButton, {marginTop: 10, marginBottom: 10}]}>
+              <Icon name={'remove'} size={24} color={'white'} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._toggleEditScheduleButtons.bind(this)}>
+            <View style={[_styles.editButton, {margin: 10}]}>
+              <Icon name={'close'} size={24} color={'white'} />
+            </View>
+          </TouchableOpacity>
+        </View>
+      );
+    } else {
+      buttons = (
+        <TouchableOpacity onPress={this._toggleEditScheduleButtons.bind(this)}>
+          <View style={_styles.editScheduleButton}>
+            <Text style={[Styles.mediumText, {color: 'white'}]}>
+              {Translations['edit_schedule']}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+
     return (
       <View style={_styles.container}>
         <SectionHeader
@@ -81,19 +142,13 @@ class ScheduleHome extends Component {
             sectionName={Translations['schedule']}
             sectionIcon={calendarIcon[1]}
             sectionIconClass={calendarIcon[0]}
-            subtitleOnClick={this._changeSchedule}
+            subtitleOnClick={this._changeSemester.bind(this)}
             subtitleName={LanguageUtils.getTranslatedName(Preferences.getSelectedLanguage(), Preferences.getCurrentSemesterInfo())}
             subtitleIcon={'arrow-swap'}
             subtitleIconClass={'ionicon'} />
         {/* TODO: replace with scroll view for schedule */}
         <View style={{flex: 1}} />
-        <TouchableOpacity onPress={this.props.editSchedule}>
-          <View style={_styles.editButton}>
-            <Text style={[Styles.mediumText, {color: 'white'}]}>
-              {Translations['edit_schedule']}
-            </Text>
-          </View>
-        </TouchableOpacity>
+        {buttons}
       </View>
     );
   };
@@ -105,16 +160,27 @@ const _styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Constants.Colors.charcoalGrey,
   },
-  editButton: {
+  editScheduleButton: {
     backgroundColor: Constants.Colors.garnet,
     height: 50,
     margin: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  editButtonText: {
-    color: 'white',
+  editButtonContainer: {
+    flexDirection: 'row',
+    height: 70,
+    width: screenWidth,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  editButton: {
+    backgroundColor: Constants.Colors.garnet,
+    height: 50,
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 // Expose component to app
