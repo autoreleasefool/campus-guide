bc1313649ffc52c557f30effc6d57919988c9204
Joseph Roque <contact@josephroque.ca>
2016-05-14 08:25:19 +0800

    Adding unit tests to ScreenUtils

---
+++ app/util/__tests__/ScreenUtils_testRootScreens.js
@@ -0,0 +1,53 @@
+/**
+ *
+ * @license
+ * Copyright (C) 2016 Joseph Roque
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Joseph Roque
+ * @file ScreenUtils_testRootScreens.js
+ * @description Tests the accessing and comparing of root screens.
+ *
+ */
+'use strict';
+
+// Unmock modules so the real module is used.
+jest.unmock('../../Constants')
+    .unmock('../ScreenUtils');
+
+describe('testRootScreens', () => {
+  it('tests the accessing and comparing of root screens.', () => {
+    const Constants = require('../../Constants');
+    const ScreenUtils = require('../ScreenUtils');
+
+    expect(ScreenUtils.getRootScreen(Constants.Views.Find.Home)).toBe(Constants.Views.Find.Home);
+    expect(ScreenUtils.getRootScreen(Constants.Views.Find.Search)).toBe(Constants.Views.Find.Home);
+    expect(ScreenUtils.getRootScreen(Constants.Views.Schedule.Home)).toBe(Constants.Views.Schedule.Home);
+    expect(ScreenUtils.getRootScreen(Constants.Views.Schedule.Editor)).toBe(Constants.Views.Schedule.Home);
+    expect(ScreenUtils.getRootScreen(Constants.Views.Discover.Home)).toBe(Constants.Views.Discover.Home);
+    expect(ScreenUtils.getRootScreen(Constants.Views.Discover.BusCampusInfo)).toBe(Constants.Views.Discover.Home);
+    expect(ScreenUtils.getRootScreen(Constants.Views.Settings.Home)).toBe(Constants.Views.Settings.Home);
+    expect(ScreenUtils.getRootScreen('invalid_room')).toBe(0);
+  });
+
+  it('tests whether certain screens are recognized as root screens or not.', () => {
+    const Constants = require('../../Constants');
+    const ScreenUtils = require('../ScreenUtils');
+
+    expect(ScreenUtils.isRootScreen(Constants.Views.Find.Home)).toBeTruthy();
+    expect(ScreenUtils.isRootScreen(Constants.Views.Find.Search)).toBeFalsy();
+    expect(ScreenUtils.isRootScreen(Constants.Views.Schedule.Home)).toBeTruthy();
+    expect(ScreenUtils.isRootScreen(Constants.Views.Discover.BusCampusInfo)).toBeFalsy();
+  });
+});
