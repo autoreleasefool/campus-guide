90981fdf80ac8fc7bc45405203bcbec5671ead67
Joseph Roque <contact@josephroque.ca>
2016-07-12 12:01:34 -0400

    Updating to react-native-maps@0.7.1

--- android/app/src/main/java/ca/josephroque/campusguide/MainActivity.java
+++ android/app/src/main/java/ca/josephroque/campusguide/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 
 import com.facebook.react.ReactActivity;
+import com.airbnb.android.react.maps.MapsPackage;
 
 public class MainActivity extends ReactActivity {
 
