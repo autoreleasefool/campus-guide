13a09d1199280c8a293232c9e7f991331d7c9dc2
Joseph Roque <contact@josephroque.ca>
2016-08-30 12:38:19 -0400

    Create more general sort/search methods

--- js/util/Database.js
+++ js/util/Database.js
@@ -32,65 +32,11 @@ import type {
 const store = require('react-native-simple-store');
 
 // Imports
+const ArrayUtils = require('ArrayUtils');
 const Promise = require('promise');
 
 /** Identifier for storing Configuration file versions.  */
 const STORE_CONFIG_VERSIONS = 'configFiles';
-
-/**
- * Sorts an array of config files by their name.
- *
- * @param {Array<ConfigFile>} configFiles an array of configuration file descriptors
- * @returns {Array<ConfigFiles>} the sorted array
- */
-function _sortConfigFiles(configFiles: Array < ConfigFile >): Array < ConfigFile > {
-  if (configFiles == null) {
-    return configFiles;
-  }
-
-  return configFiles.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-
-    // names must be equal
-    return 0;
-  });
-}
-
-/**
- * Search an array of configuration file descriptors to see if the file already exists.
- *
- * @param {Array<ConfigFile>} configFiles a sorted array of configuration file descriptors
- * @param {ConfigFile} file               the file to search for
- * @returns {number} the index of the file in the array, or -1 if the file was not found
- */
-function _searchConfigFiles(configFiles: Array < ConfigFile >, file: ConfigFile): number {
-  const fileName = file.name.toUpperCase();
-
-  let low: number = 0;
-  let high: number = configFiles.length - 1;
-  while (low <= high) {
-    const mid: number = Math.floor((low + high) / 2);
-    const midName = configFiles[mid].name.toUpperCase();
-
-    if (midName < fileName) {
-      low = mid + 1;
-    } else if (midName > fileName) {
-      high = mid - 1;
-    } else {
-      return mid;
-    }
-  }
-
-  return -1;
-}
 
 /**
  * Gets a list of config files and their versions from the database.
@@ -100,7 +46,7 @@ function _searchConfigFiles(configFiles: Array < ConfigFile >, file: ConfigFile)
 export function getConfigVersions(): Promise < Array < ConfigFile > > {
   return store.get(STORE_CONFIG_VERSIONS)
       .then(configVersions => {
-        return _sortConfigFiles(configVersions);
+        return ArrayUtils.sortObjectArrayByKeyValues(configVersions, 'name');
       });
 }
 
@@ -116,11 +62,11 @@ export function updateConfigVersions(updatedConfigFiles: Array < ConfigFile >): 
         let updated = updatedConfigFiles;
 
         if (configFiles != null) {
-          const sortedConfigFiles = _sortConfigFiles(configFiles);
+          const sortedConfigFiles = ArrayUtils.sortObjectArrayByKeyValues(configFiles, 'name');
 
           // Replace any config versions that should be updated
           for (let i = 0; i < updatedConfigFiles.length; i++) {
-            const index = _searchConfigFiles(sortedConfigFiles, updatedConfigFiles[i]);
+            const index = ArrayUtils.searchObjectArrayByKeyValue(sortedConfigFiles, 'name', updatedConfigFiles[i].name);
             if (index >= 0) {
               sortedConfigFiles.splice(index, 1);
             }
