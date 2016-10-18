62cbd8c55713e639c6dec18c2a5656cc616fa24e
Joseph Roque <contact@josephroque.ca>
2016-10-18 09:21:48 -0400

    Add action and reducer tests

---
+++ src/actions/__tests__/navigation-test.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * @license
+ * Copyright (C) 2016 Joseph Roque
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Joseph Roque
+ * @created 2016-10-17
+ * @file navigation-test.js
+ * @description Tests navigation actions
+ *
+ */
+'use strict';
+
+// Types
+import type {
+  Tab,
+} from 'types';
+
+// Imports
+import * as actions from '../navigation';
+
+describe('navigation actions', () => {
+  it('should create an action to switch the tabs', () => {
+    const tab: Tab = 'find';
+    const expectedAction = {
+      type: 'SWITCH_TAB',
+      tab,
+    };
+
+    expect(actions.switchTab(tab)).toEqual(expectedAction);
+  });
+});
---
+++ src/actions/__tests__/update-test.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * @license
+ * Copyright (C) 2016 Joseph Roque
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Joseph Roque
+ * @created 2016-10-17
+ * @file update-test.js
+ * @description Tests update actions
+ *
+ */
+'use strict';
+
+// Types
+import type {
+  Update,
+} from 'types';
+
+// Imports
+import * as actions from '../update';
+
+describe('navigation actions', () => {
+  it('should create an action to update the download progress', () => {
+    const update: Update = {
+      currentDownload: 'download.jpg',
+      filesDownloaded: [
+        'download1.jpg',
+        'download2.jpg',
+      ],
+      totalFiles: 1,
+    };
+
+    const expectedAction = {
+      type: 'UPDATE_PROGRESS',
+      update,
+    };
+
+    expect(actions.updateProgress(update)).toEqual(expectedAction);
+  });
+});
