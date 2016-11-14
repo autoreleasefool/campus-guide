83f5217132f444fb4ad148bf51d7187586a691e3
Joseph Roque <contact@josephroque.ca>
2016-11-13 23:16:09 -0500

    Settings handles links and text

--- src/containers/settings/Settings.js
+++ src/containers/settings/Settings.js
@@ -27,13 +27,18 @@
 // React imports
 import React from 'react';
 import {
+  Alert,
+  Clipboard,
+  Linking,
   ListView,
+  Modal,
   Platform,
   StyleSheet,
   Switch,
   Text,
   TouchableOpacity,
   View,
+  WebView,
 } from 'react-native';
 
 // Redux imports
@@ -63,14 +68,25 @@ type Props = {
 
 // Type definition for component state.
 type State = {
-  dataSource: ListView.DataSource,  // List of settings to display
+  dataSource: ListView.DataSource,          // List of settings to display
+  listModalDataSource: ListView.DataSource, // List of data to display in modal
+  listModalTitle: string,                   // Title of the list view modal
+  listModalVisible: boolean,                // Indicates if the list modal should be visible
+  url: string,                              // URL to visit if WebView modal is visible
+  webViewModalVisible: boolean,             // Indicates if the web rendering modal should be visible
 };
 
 // Imports
+import DeviceInfo from 'react-native-device-info';
 import Header from 'Header';
 import * as Configuration from 'Configuration';
 import * as Constants from 'Constants';
-import {loadTranslations, unloadTranslations, getTranslations, getTranslatedName} from 'TranslationUtils';
+import * as ExternalUtils from 'ExternalUtils';
+import * as TextUtils from 'TextUtils';
+import * as TranslationUtils from 'TranslationUtils';
+
+// Modal padding on iOS
+const HEADER_PADDING_IOS: number = 25;
 
 // Default opacity for tap when setting is not a boolean
 const DEFAULT_OPACITY: number = 0.4;
@@ -99,6 +115,14 @@ class Settings extends React.Component {
         rowHasChanged: (r1: any) => this._keyChanged === r1.key || this._keyChanged === 'pref_lang',
         sectionHeaderHasChanged: (s1: any, s2: any) => s1 !== s2 || this._keyChanged === 'pref_lang',
       }),
+      listModalDataSource: new ListView.DataSource({
+        rowHasChanged: (r1: any, r2: any) => r1 !== r2,
+        sectionHeaderHasChanged: (s1: any, s2: any) => s1 !== s2,
+      }),
+      listModalTitle: '',
+      listModalVisible: false,
+      url: '',
+      webViewModalVisible: false,
     };
   }
 
@@ -107,8 +131,8 @@ class Settings extends React.Component {
    */
   componentDidMount(): void {
     Configuration.init()
-        .then(() => loadTranslations('en'))
-        .then(() => loadTranslations('fr'))
+        .then(() => TranslationUtils.loadTranslations('en'))
+        .then(() => TranslationUtils.loadTranslations('fr'))
         .then(() => Configuration.getConfig('/settings.json'))
         .then((configSettings: Object) => {
           this._settings = configSettings;
@@ -143,7 +167,7 @@ class Settings extends React.Component {
    * Unloads the unused language.
    */
   componentWillUnmount(): void {
-    unloadTranslations(this.props.language === 'en' ? 'fr' : 'en');
+    TranslationUtils.unloadTranslations(this.props.language === 'en' ? 'fr' : 'en');
   }
 
   /** Set of settings to display. */
@@ -173,6 +197,18 @@ class Settings extends React.Component {
   }
 
   /**
+   * Closes all modals.
+   */
+  _closeModal(): void {
+    this.setState({
+      listModalDataSource: this.state.listModalDataSource.cloneWithRowsAndSections({}),
+      listModalVisible: false,
+      url: '',
+      webViewModalVisible: false,
+    });
+  }
+
+  /**
    * Returns the value of a setting based on the provided key. The returned value may be a string, boolean, integer,
    * or object, and should correspond to the type of the setting.
    *
@@ -187,12 +223,16 @@ class Settings extends React.Component {
           : 'Français';
       case 'pref_wheel':
         return this.props.prefersWheelchair;
-      case 'pref_semester':
-        return getTranslatedName(this.props.language, this.props.semesters[this.props.currentSemester]);
+      case 'pref_semester': {
+        const semester = this.props.semesters[this.props.currentSemester];
+        return TranslationUtils.getTranslatedName(this.props.language, semester);
+      }
       case 'pref_search_all_always':
         return this.props.alwaysSearchAll;
       case 'pref_time_format':
         return this.props.timeFormat;
+      case 'app_version':
+        return DeviceInfo.getVersion();
       default:
         return null;
     }
@@ -206,6 +246,21 @@ class Settings extends React.Component {
   _onPressRow(setting: Object): void {
     if (setting.type === 'boolean') {
       // Ignore boolean settings, they can only be manipulated by switch
+      return;
+    } else if (setting.type === 'link') {
+      // Get current language for translations
+      const Translations: Object = TranslationUtils.getTranslations(this.props.language);
+
+      // Open the provided link
+      const link = TranslationUtils.getTranslatedVariant(this.props.language, 'link', setting);
+      ExternalUtils.openLink(
+        link || ExternalUtils.getDefaultLink(),
+        Translations,
+        Linking,
+        Alert,
+        Clipboard,
+        TextUtils,
+      );
       return;
     }
 
@@ -223,9 +278,105 @@ class Settings extends React.Component {
       this.props.updateConfiguration({alwaysSearchAll: !this.props.alwaysSearchAll});
     } else if (setting.key === 'pref_time_format') {
       this.props.updateConfiguration({preferredTimeFormat: this.props.timeFormat === '12h' ? '24h' : '12h'});
+    } else if (setting.key === 'app_open_source') {
+      const licenses = require('../../../assets/json/licenses.json');
+      this.setState({
+        listModalDataSource: this.state.listModalDataSource.cloneWithRowsAndSections(licenses),
+        listModalTitle: TranslationUtils.getTranslatedName(this.props.language, setting) || '',
+        listModalVisible: true,
+      });
     }
 
-    this._keyChanged = key;
+    this._keyChanged = setting.key;
+  }
+
+  /**
+   * Renders a button to close modals.
+   *
+   * @returns {ReactElement<any>} view to close modal
+   */
+  _renderCloseModalButton(): ReactElement < any > {
+    // Get current language for translations
+    const Translations: Object = TranslationUtils.getTranslations(this.props.language);
+
+    return (
+      <View style={_styles.modalCloseContainer}>
+        <TouchableOpacity onPress={this._closeModal.bind(this)}>
+          <Text style={_styles.modalCloseText}>{Translations.done}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  /**
+   * Renders content for the list view modal.
+   *
+   * @returns {ReactElement<any>} a list view with the rows and sections loaded
+   */
+  _renderListModal(): ReactElement < any > {
+    let title = null;
+    if (this.state.listModalTitle.length > 0) {
+      title = (
+        <View style={[_styles.setting, _styles.modalListTitle]}>
+          <Text style={[_styles.settingText, _styles.modalListTitleText]}>{this.state.listModalTitle}</Text>
+        </View>
+      );
+    }
+
+    return (
+      <View style={[_styles.container, {backgroundColor: Constants.Colors.primaryBackground}]}>
+        {title}
+        <ListView
+            dataSource={this.state.listModalDataSource}
+            renderRow={this._renderListModalRow.bind(this)}
+            renderSectionHeader={this._renderListModalSectionHeader.bind(this)}
+            style={_styles.modalListView} />
+        {this._renderCloseModalButton()}
+      </View>
+    );
+  }
+
+  /**
+   * Displays a single row, representing an item in the listview.
+   *
+   * @param {string} item text to be rendered
+   * @returns {ReactElement<any>} views to render the setting in the list
+   */
+  _renderListModalRow(item: string): ReactElement < any > {
+    return (
+      <Text style={_styles.modalListViewText}>{item}</Text>
+    );
+  }
+
+  /**
+   * Renders a heading for a section in the modal's list view.
+   *
+   * @param {Object} sectionData section contents
+   * @param {string} sectionName index of the section
+   * @returns {ReactElement<any>} a {SectionHeader} with the name of the section
+   */
+  _renderListModalSectionHeader(sectionData: Object, sectionName: string): ReactElement < any > {
+    return (
+      <View style={_styles.modalListViewHeader}>
+        <Header title={sectionName} />
+      </View>
+    );
+  }
+
+  /**
+   * Renders content for the webview modal.
+   *
+   * @returns {ReactElement<any>} a webview with the set URL or HTML loaded
+   */
+  _renderWebViewModal(): ReactElement < any > {
+    let source = null;
+    if (this.state.webViewModalVisible) {
+      source = {uri: this.state.url};
+    }
+
+    return (
+      <WebView source={source} />
+    );
   }
 
   /**
@@ -258,7 +409,7 @@ class Settings extends React.Component {
             activeOpacity={setting.type === 'boolean' ? 1 : DEFAULT_OPACITY}
             onPress={this._onPressRow.bind(this, setting)}>
           <View style={_styles.setting}>
-            <Text style={_styles.settingText}>{getTranslatedName(this.props.language, setting)}</Text>
+            <Text style={_styles.settingText}>{TranslationUtils.getTranslatedName(this.props.language, setting)}</Text>
             {content}
           </View>
         </TouchableOpacity>
@@ -316,6 +467,22 @@ class Settings extends React.Component {
 
     return (
       <View style={_styles.container}>
+        <Modal
+            animationType={'slide'}
+            transparent={false}
+            visible={this.state.listModalVisible}
+            onRequestClose={this._closeModal.bind(this)}>
+          {this._renderListModal()}
+        </Modal>
+
+        <Modal
+            animationType={'slide'}
+            transparent={false}
+            visible={this.state.webViewModalVisible}
+            onRequestClose={this._closeModal.bind(this)}>
+          {this._renderWebViewModal()}
+        </Modal>
+
         <View style={_styles.headerContainer}>
           <Header
               icon={{name: Platform.OS === 'ios' ? 'ios-settings' : 'settings', class: 'ionicon'}}
@@ -369,6 +536,36 @@ const _styles = StyleSheet.create({
   settingValue: {
     color: Constants.Colors.primaryBlackText,
     fontSize: Constants.Sizes.Text.Body,
+  },
+  modalListView: {
+    backgroundColor: Constants.Colors.secondaryBackground,
+  },
+  modalListViewHeader: {
+    backgroundColor: Constants.Colors.secondaryBackground,
+  },
+  modalListViewText: {
+    color: Constants.Colors.primaryWhiteText,
+    fontSize: Constants.Sizes.Text.Caption,
+    margin: Constants.Sizes.Margins.Regular,
+  },
+  modalListTitle: {
+    marginTop: Platform.OS === 'ios' ? HEADER_PADDING_IOS : 0,
+    backgroundColor: Constants.Colors.primaryBackground,
+  },
+  modalListTitleText: {
+    color: Constants.Colors.primaryWhiteText,
+  },
+  modalCloseContainer: {
+    backgroundColor: Constants.Colors.secondaryBackground,
+    borderTopColor: Constants.Colors.darkTransparentBackground,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    justifyContent: 'flex-end',
+  },
+  modalCloseText: {
+    margin: Constants.Sizes.Margins.Expanded,
+    color: Constants.Colors.primaryWhiteText,
+    fontSize: Constants.Sizes.Text.Body,
+    textAlign: 'right',
   },
 });
 
