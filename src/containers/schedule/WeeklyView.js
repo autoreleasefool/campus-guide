eaf344312dd6b1ef75ed1c114d0099f81a0b0d8c
Joseph Roque <contact@josephroque.ca>
2017-01-29 18:15:43 -0500

    Semester picker for weekly view

--- src/containers/schedule/WeeklyView.js
+++ src/containers/schedule/WeeklyView.js
@@ -27,18 +27,158 @@
 // React imports
 import React from 'react';
 import {
+  // Dimensions,
+  LayoutAnimation,
+  Picker,
+  Platform,
   StyleSheet,
-  Text,
+  // TouchableOpacity,
   View,
 } from 'react-native';
 
-import * as Constants from 'Constants';
+// Redux imports
+import {connect} from 'react-redux';
+import {
+  updateConfiguration,
+} from 'actions';
 
-export default class Weekly extends React.Component {
+// Type imports
+import type {
+  ConfigurationOptions,
+  Language,
+  Semester,
+  TimeFormat,
+} from 'types';
+
+// Type definition for component props.
+type Props = {
+  currentSemester: number,                                // The current semester, selected by the user
+  language: Language,                                     // The current language, selected by the user
+  semesters: Array < Semester >,                          // Semesters available at the university
+  timeFormat: TimeFormat,                                 // The user's preferred time format
+  updateConfiguration: (o: ConfigurationOptions) => void, // Update the global configuration state
+};
+
+// Type definition for component state.
+type State = {
+  showSemesters: boolean, // True to show drop down to swap semesters
+};
+
+import Header from 'Header';
+import * as Constants from 'Constants';
+import * as TranslationUtils from 'TranslationUtils';
+import {getPlatformIcon} from 'DisplayUtils';
+
+// Screen dimensions
+// const screenWidth = Dimensions.get('window').width;
+
+// Icon for representing the current semester
+const semesterIcon = {
+  android: {
+    class: 'ionicons',
+    name: 'md-calendar',
+  },
+  ios: {
+    class: 'ionicons',
+    name: 'ios-calendar-outline',
+  },
+};
+
+class Weekly extends React.Component {
+
+  /**
+   * Properties this component expects to be provided by its parent.
+   */
+  props: Props;
+
+  /**
+   * Current state of the component.
+   */
+  state: State;
+
+  /**
+   * Constructor.
+   *
+   * @param {props} props component props
+   */
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      showSemesters: false,
+    };
+
+    (this:any)._toggleSwitchSemester = this._toggleSwitchSemester.bind(this);
+  }
+
+  /**
+   * Updates the current semester.
+   *
+   * @param {number} semester the semester to switch to
+   */
+  _switchSemester(semester: number): void {
+    this.props.updateConfiguration({currentSemester: semester});
+  }
+
+  /**
+   * Toggles the drop down to switch semesters.
+   */
+  _toggleSwitchSemester(): void {
+    LayoutAnimation.easeInEaseOut();
+    this.setState({showSemesters: !this.state.showSemesters});
+  }
+
+  /**
+   * Renders the current semester and a drop down to switch semesters.
+   *
+   * @param {Object} Translations translations in the current language of certain text
+   * @returns {ReactElement<any>} the elements to render
+   */
+  _renderSemesters(Translations: Object): ReactElement < any > {
+    const semesterName = TranslationUtils.getTranslatedName(
+      this.props.language,
+      this.props.semesters[this.props.currentSemester]
+    ) || '';
+
+    return (
+      <View>
+        <Header
+            icon={getPlatformIcon(Platform.OS, semesterIcon)}
+            subtitle={(this.state.showSemesters ? Translations.cancel : Translations.switch)}
+            subtitleCallback={this._toggleSwitchSemester}
+            title={semesterName} />
+        {this.state.showSemesters
+          ?
+            <Picker
+                itemStyle={_styles.semesterItem}
+                selectedValue={this.props.currentSemester}
+                onValueChange={(value) => this._switchSemester(value)}>
+              {this.props.semesters.map((semester, index) => {
+                const name = TranslationUtils.getTranslatedName(this.props.language, semester);
+                return (
+                  <Picker.Item
+                      key={name}
+                      label={name}
+                      value={index} />
+                );
+              })}
+            </Picker>
+          : null}
+      </View>
+    );
+  }
+
+  /**
+   * Renders a list of the user's courses, organized by days.
+   *
+   * @returns {ReactElement<any>} the hierarchy of views to render
+   */
   render(): ReactElement < any > {
+    // Get current language for translations
+    const Translations: Object = TranslationUtils.getTranslations(this.props.language);
+
     return (
       <View style={_styles.container}>
-        <Text>{'Weekly'}</Text>
+        {this._renderSemesters(Translations)}
       </View>
     );
   }
@@ -50,4 +190,26 @@ const _styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Constants.Colors.garnet,
   },
+  semesterItem: {
+    color: Constants.Colors.primaryWhiteText,
+  },
 });
+
+// Map state to props
+const select = (store) => {
+  return {
+    currentSemester: store.config.currentSemester,
+    language: store.config.language,
+    semesters: store.config.semesters,
+    timeFormat: store.config.preferredTimeFormat,
+  };
+};
+
+// Map dispatch to props
+const actions = (dispatch) => {
+  return {
+    updateConfiguration: (options: ConfigurationOptions) => dispatch(updateConfiguration(options)),
+  };
+};
+
+export default connect(select, actions)(Weekly);
