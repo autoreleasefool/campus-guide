20e41c2fd145df5a9374352f2de644a27c2e679d
Joseph Roque <contact@josephroque.ca>
2017-07-06 18:31:03 -0700

    Add react-native-zip-archive

--- android/app/src/main/java/ca/josephroque/campusguide/MainApplication.java
+++ android/app/src/main/java/ca/josephroque/campusguide/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.rnziparchive.RNZipArchivePackage;
 import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.rnfs.RNFSPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNZipArchivePackage(),
           new SnackbarPackage(),
           new RNFSPackage(),
           new RNDeviceInfo(),
