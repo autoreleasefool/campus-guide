e0c8142a34e71d2fd6c711035a3eb94e190bbbe1
Joseph Roque <contact@josephroque.ca>
2017-11-15 01:02:49 -0500

    Setup Fabric for Android

--- android/app/src/main/java/ca/josephroque/campusguide/MainActivity.java
+++ android/app/src/main/java/ca/josephroque/campusguide/MainActivity.java
@@ -1,6 +1,10 @@
 package ca.josephroque.campusguide;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "CampusGuide";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
     }
 }
