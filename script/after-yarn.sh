45f906b3e002c771e0bc71210605dd1a7a4a6227
Joseph Roque <contact@josephroque.ca>
2018-01-02 16:33:51 -0500

    Clean up script

--- script/after-yarn.sh
+++ script/after-yarn.sh
@@ -14,12 +14,5 @@ sed -i '' '/setImmediate/ { d; }' ./node_modules/@types/node/index.d.ts
 sed -i '' '/clearImmediate/ { d; }' ./node_modules/@types/node/index.d.ts
 sed -i '' '/declare var require/ { d; }' ./node_modules/@types/node/index.d.ts
 
-# sed -i '' '/setTimeout|/d' ./node_modules/@types/node/index.d.ts
-# sed -i '' '/clearTimeout|/d' ./node_modules/@types/node/index.d.ts
-# sed -i '' '/setInterval|/d' ./node_modules/@types/node/index.d.ts
-# sed -i '' '/clearInterval|/d' ./node_modules/@types/node/index.d.ts
-# sed -i '' '/setImmediate|/d' ./node_modules/@types/node/index.d.ts
-# sed -i '' '/clearImmediate|/d' ./node_modules/@types/node/index.d.ts
-
 # # Fix broken library
-# sed -i '' 's/domain = require/\/\/domain = require/g' ./node_modules/asap/raw.js
+sed -i '' 's/domain = require/\/\/domain = require/g' ./node_modules/asap/raw.js
